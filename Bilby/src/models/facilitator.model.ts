405ac4747df5be2b54f3d91c8b31165a65608a15
NikhilChitale <38122716+NikhilChitale@users.noreply.github.com>
2020-04-06 12:54:27 +1000

    changes
    changes

--- Bilby/src/models/facilitator.model.ts
+++ Bilby/src/models/facilitator.model.ts
@@ -7,9 +7,63 @@ export class Facilitator extends Typegoose {
   @prop({ required: true, ref: City })
   public city!: Ref<City>;
 
+  /* added by Nikhil*/
   @arrayProp({ required: true, items: String })
-  public trained!: string[];
+  /*public trained!: string[];*/ 
+  public P123!: string[];
 
+    /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public P456!: string[];
+
+    /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public DHD!: string[];
+
+    /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public HHI!: string[];
+
+  /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public CSE!: string[];
+
+  /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public Pe!: string[];
+
+  /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public DHDe!: string[];
+
+  /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public DADe!: string[];
+  
+
+    /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public HHIe!: string[];
+
+    /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public CSEe!: string[];
+    
+  /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public TBIdea!: string[];
+
+  /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public Ah!: string[];
+
+  
+
+
+    /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public C!: string[];
+    
   @prop({ required: true })
   public reliable!: boolean;
 
--- Bilby/src/models/guestSpeaker.model.ts
+++ Bilby/src/models/guestSpeaker.model.ts
@@ -22,6 +22,61 @@ export class GuestSpeaker extends Typegoose {
   @arrayProp({ required: true, items: Object })
   public assignedTimes!: Availability[];
 
+  /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  /*public trained!: string[];*/ 
+  public P123!: string[];
+
+    /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public P456!: string[];
+
+    /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public DHD!: string[];
+
+    /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public HHI!: string[];
+
+  /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public CSE!: string[];
+
+  /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public Pe!: string[];
+
+  /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public DHDe!: string[];
+
+  /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public DADe!: string[];
+  
+
+    /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public HHIe!: string[];
+
+    /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public CSEe!: string[];
+    
+  /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public TBIdea!: string[];
+
+  /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public Ah!: string[];
+
+    /* added by Nikhil*/
+  @arrayProp({ required: true, items: String })
+  public C!: string[];
+  
+
 }
 
 export const GuestSpeakerModel = new GuestSpeaker().getModelForClass(GuestSpeaker);
